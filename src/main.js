c6bcc069e00791af2fbb79991a0d39464ada1d2e
Abdurahmon <abduqodirovabdurakhmon@gmail.com>
2025-01-23 11:45:06 +0500

    nice

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "./router";
-import { createVfm } from 'vue-final-modal'
-
-const vfm = createVfm()
 
 createApp(App)
   .use(router)
